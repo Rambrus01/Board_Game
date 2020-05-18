4e91142bf20e22d26bcdb8d476d27369d19b6db7
Rambrus01 <rambrus0@gmail.com>
2020-05-18 15:42:48 +0200

    Player class created, position and position getter added.

---
+++ src/Model/Player/Player.java
@@ -0,0 +1,18 @@
+package Model.Player;
+
+public class Player {
+
+    // The player should have a position, should be able to change its position.
+
+    private int positionOfPlayer = 0;
+
+    public Player() {
+    }
+
+    public void changePlayersPosition(){
+    }
+
+    public int getPositionOfPlayer() {
+        return this.positionOfPlayer;
+    }
+}
