019bc7ed3167037d7a8e983babec00398222fbd7
Rambrus01 <rambrus0@gmail.com>
2020-05-18 21:22:24 +0200

    updated explanation.

--- src/Model/Player/Player.java
+++ src/Model/Player/Player.java
@@ -4,6 +4,7 @@ public class Player {
 
     // The player should have a position, should be able to change its position.
 
+    // This variable saves the players current position on a line based board,
     private int positionOfPlayer = 0;
 
     public Player() {
