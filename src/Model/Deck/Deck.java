d6034b7d062970ccfb12ed62df0fdb58b0cc1614
Rambrus01 <rambrus0@gmail.com>
2020-05-18 15:05:57 +0200

    Create Deck function implemented, card value implemented.

--- src/Model/Deck/Deck.java
+++ src/Model/Deck/Deck.java
@@ -1,6 +1,27 @@
 package Model.Deck;
 
-public class Deck {
+import java.util.ArrayList;
 
-    Card card = new Card();
+public class Deck {
+    // This class will manage the deck, where the players will be drawing from.
+
+    //Attributes
+    private ArrayList<Card> deck = new ArrayList<>();
+    private int[] valueofCard = new int[] {1,2};
+
+    public Deck() {
+        this.createDeck();
+    }
+
+    //Getters
+    public ArrayList<Card> getDeck() {
+        return this.deck;
+    }
+
+    public void createDeck() {
+        for (int index = 0; index < valueofCard.length; index++) {
+            deck.add(new Card(valueofCard[index]));
+        }
+    }
+
 }
